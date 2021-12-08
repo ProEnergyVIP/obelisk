2edcd3e371a1b662efe968dd4b7435a725122c21
Eric Wong <ericsyw@gmail.com>
2021-12-08 13:33:24 +0800

    Update to new custom reflex-platform to use nixpkgs-21.05 and ghc/ghcjs 8.10.7

--- haskell-overlays/misc-deps.nix
+++ haskell-overlays/misc-deps.nix
@@ -23,7 +23,7 @@ in
   # https://github.com/haskell/hackage-security/issues/247
   hackage-security = haskellLib.dontCheck super.hackage-security; # only tests use aeson and are not compat with 1.5;
   heist = haskellLib.doJailbreak super.heist; # aeson 1.5 bump
-  aeson = super.aeson_1_5_2_0; # TODO: bump reflex-platform aeson to 1.5
+  #aeson = super.aeson_1_5_2_0; # TODO: bump reflex-platform aeson to 1.5
   aeson-gadt-th = haskellLib.doJailbreak super.aeson-gadt-th; # requires aeson 1.5 for ghc8.10 support?
 
   snap = haskellLib.doJailbreak super.snap;
@@ -31,7 +31,12 @@ in
   snap-core = haskellLib.dontCheck (self.callCabal2nix "snap-core" (hackGet ../dep/snap-core) {});
 
   logging-effect = haskellLib.doJailbreak (self.callHackage "logging-effect" "1.3.10" {});
-  # unliftio-core = self.callHackage "unliftio-core" "0.2.0.1" {};
+  unliftio-core = self.callHackage "unliftio-core" "0.1.2.0" {};
   # shelly = self.callHackage "shelly" "1.9.0" {};
   # monad-logger = self.callHackage "monad-logger" "0.3.36" {};
+  # megaparsec = self.callHackage "megaparsec" "8.0.0" {};
+  neat-interpolation = self.callHackage "neat-interpolation" "0.5.1.2" {};
+
+  hnix-store-core = self.callHackage "hnix-store-core" "0.4.2.0" {};
+  hnix-store-remote = self.callHackage "hnix-store-remote" "0.4.2.0" {};
 }
