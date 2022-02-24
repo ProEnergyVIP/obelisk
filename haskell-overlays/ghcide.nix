ed1f1e8ee31737dfd4d51357d251da439fd0fa2c
eric.wong <ericsyw@gmail.com>
2022-02-24 23:16:19 +0800

    update

--- haskell-overlays/ghcide.nix
+++ haskell-overlays/ghcide.nix
@@ -7,8 +7,8 @@ let
 in {
   ghcide = justStaticExecutables (dontCheck (callHackageDirect {
     pkg = "ghcide";
-    ver = "0.2.0";
-    sha256 = "199l4qzrghhz6wbfkgqdl4gll4wvgpr190kinzhv88idnn9pxm96";
+    ver = "1.5.0.1";
+    sha256 = "ec0970df3e04283f0ad74b957720090ddeb900ce0b3d6c997a6c466e6ec2ce7c";
   } rec {
     lsp-test = dontCheck (callHackage "lsp-test" "0.14.0.0" { });
   }));
