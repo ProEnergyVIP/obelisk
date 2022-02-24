733b5729ec743567ad53d0e8557d0eb11af18fac
eric.wong <ericsyw@gmail.com>
2022-02-24 23:19:53 +0800

    update

--- haskell-overlays/ghcide.nix
+++ haskell-overlays/ghcide.nix
@@ -5,11 +5,5 @@ let
   inherit (pkgs.haskellPackages) callHackageDirect callHackage;
 
 in {
-  ghcide = justStaticExecutables (dontCheck (callHackageDirect {
-    pkg = "ghcide";
-    ver = "1.5.0.1";
-    sha256 = "ec0970df3e04283f0ad74b957720090ddeb900ce0b3d6c997a6c466e6ec2ce7c";
-  } rec {
-    lsp-test = dontCheck (callHackage "lsp-test" "0.14.0.0" { });
-  }));
+  ghcide = justStaticExecutables (dontCheck (callHackage "ghcide" "1.5.0.1" {}));
 }
