3f2d2fc449c7db77dcc65006029cffdfd33a233f
eric.wong <ericsyw@gmail.com>
2022-08-06 01:04:45 +0800

    Don't let closure compiler check variables

--- default.nix
+++ default.nix
@@ -87,7 +87,7 @@ in rec {
       ${if optimizationLevel == null then ''
         ln -s "$dir/all.unminified.js" "$dir/all.js"
       '' else ''
-        '${pkgs.closurecompiler}/bin/closure-compiler' --externs '${reflex-platform.ghcjsExternsJs}' -O '${optimizationLevel}' --jscomp_warning=checkVars --warning_level=QUIET --js_output_file="$dir/all.js" "$dir/all.unminified.js"
+        '${pkgs.closurecompiler}/bin/closure-compiler' --externs '${reflex-platform.ghcjsExternsJs}' -O '${optimizationLevel}' --warning_level=QUIET --js_output_file="$dir/all.js" "$dir/all.unminified.js"
       ''}
     done
   '';
